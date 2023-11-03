f4d87d2fd204c39dc9528e5d940bebb4e1ef8d2c
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-11-03 09:35:43 +0100

    TCR

--- chess_game_test.py
+++ chess_game_test.py
@@ -3,7 +3,7 @@ import chess_game
 
 
 class ChessGameTest(unittest.TestCase):
-    def test_something(self):
+    def from_to_positions_and_figure_type_is_enough_to_make_turn(self):
         result = chess_game.ChessGame().turn('f3', 'e5', 'knight')
         self.assertEqual(result, True)
 

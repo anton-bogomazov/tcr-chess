87e16439d0aa4a96acd29ec95861c086a2eabdf3
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-11-03 09:55:09 +0100

    TCR

--- chess_game_test.py
+++ chess_game_test.py
@@ -9,7 +9,7 @@ class ChessGameTest(unittest.TestCase):
 
     def fail_to_make_turn_if_every_arg_is_not_provided(self):
         with self.assertRaises(TypeError):
-            chess_game.ChessGame().turn('f3', 'knight')
+            chess_game.ChessGame().turn('f3', 'knight', 'e4')
 
 
 if __name__ == '__main__':

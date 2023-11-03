9ab6199cbd4c5657e77bc8837c35bb89d381a064
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-11-03 10:00:58 +0100

    TCR

--- chess_game_test.py
+++ chess_game_test.py
@@ -3,13 +3,13 @@ import chess_game
 
 
 class ChessGameTest(unittest.TestCase):
-    def from_to_positions_and_figure_type_is_enough_to_make_turn(self):
+    def test_from_to_positions_and_figure_type_is_enough_to_make_turn(self):
         result = chess_game.ChessGame().turn('f3', 'e5', 'knight')
         self.assertEqual(result, True)
 
-    def fail_to_make_turn_if_every_arg_is_not_provided(self):
+    def test_fail_to_make_turn_if_every_arg_is_not_provided(self):
         with self.assertRaises(TypeError):
-            chess_game.ChessGame().turn('f3', 'knight', 'e4')
+            chess_game.ChessGame().turn('f3', 'knight')
 
 
 if __name__ == '__main__':

85a926c2b053e5bb4c9f4a6a5b52751446307eaf
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-11-03 09:54:29 +0100

    TCR

--- chess_game.py
+++ chess_game.py
@@ -1,4 +1,9 @@
 class ChessGame:
-    def turn(self, fr, to, figure):
+    def turn(self, fr=None, to=None, figure=None):
+        if fr is None:
+            raise TypeError('"from" should be a string')
+        if to is None:
+            raise TypeError('"to" should be a string')
+        if figure is None:
+            raise TypeError('"figure" should be a string')
         return True
-
--- chess_game_test.py
+++ chess_game_test.py
@@ -7,6 +7,10 @@ class ChessGameTest(unittest.TestCase):
         result = chess_game.ChessGame().turn('f3', 'e5', 'knight')
         self.assertEqual(result, True)
 
+    def fail_to_make_turn_if_every_arg_is_not_provided(self):
+        with self.assertRaises(TypeError):
+            chess_game.ChessGame().turn('f3', 'knight')
+
 
 if __name__ == '__main__':
     unittest.main()

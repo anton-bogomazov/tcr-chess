015c188b1a7dc31355a9288294b26f275bc89aca
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-11-04 12:46:39 +0100

    TCR

--- chess_game.py
+++ chess_game.py
@@ -109,7 +109,17 @@ class King(ChessFigure):
 
 class Queen(ChessFigure):
     def turns(self):
-        raise NotImplementedError
+        literal, numeral = self.position
+        rook_turns = [(chr(ord(literal) - i), numeral) for i in range(1, 8)] +\
+                [(chr(ord(literal) + i), numeral) for i in range(1, 8)] +\
+                [(chr(ord(literal)), numeral + i) for i in range(1, 8)] +\
+                [(chr(ord(literal)), numeral - i) for i in range(1, 8)]
+        bishop_turns = [(chr(ord(literal) - i), numeral - i) for i in range(1, 8)] +\
+                [(chr(ord(literal) + i), numeral + i) for i in range(1, 8)] +\
+                [(chr(ord(literal) - i), numeral + i) for i in range(1, 8)] +\
+                [(chr(ord(literal) + i), numeral - i) for i in range(1, 8)]
+        
+        return set(filter(lambda t: not self.is_out_of_board(*t), rook_turns + bishop_turns)) - {self.position}
 
 
 class Rook(ChessFigure):
--- chess_game_test.py
+++ chess_game_test.py
@@ -117,5 +117,23 @@ class ChessGameTest(unittest.TestCase):
             ('h', 3), ('c', 8),
         })
 
+    def test_queen_possible_turns_is_a_combination_of_rook_and_bishop_turns(self):
+        rook = chess_game.Queen(('c', 3), chess_game.Color.WHITE)
+        self.assertEqual(len(rook.turns()), 25)
+        self.assertEqual(rook.turns(), {
+            ('a', 3), ('c', 1),
+            ('b', 3), ('c', 2),
+            ('d', 3), ('c', 4),
+            ('e', 3), ('c', 5),
+            ('f', 3), ('c', 6),
+            ('g', 3), ('c', 7),
+            ('h', 3), ('c', 8),
+        }.union(
+            {
+                ('a', 1), ('b', 2), ('d', 2), ('e', 1), ('f', 6),
+                ('a', 5), ('b', 4), ('d', 4), ('e', 5), ('g', 7), ('h', 8),
+            }
+        ))
+
 if __name__ == '__main__':
     unittest.main()

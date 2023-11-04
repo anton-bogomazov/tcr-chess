9a0b60faf8f1445db45bb7346dad64782546b9e6
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-11-04 12:55:56 +0100

    extract figures

---
+++ chess_figures.py
@@ -0,0 +1,101 @@
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+
+@dataclass()
+class ChessFigure(ABC):
+    def __init__(self, position, color):
+        self.color = color
+        self.position = position
+        self.touched = False
+
+    @abstractmethod
+    def turns(self):
+        raise NotImplementedError
+
+    def move(self, to):
+        if to in self.turns():
+            self.position = to
+        self.touched = True
+
+    def is_out_of_board(self, literal, numeral):
+        return literal not in ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h') or numeral > 8 or numeral <= 0
+
+
+class King(ChessFigure):
+    def turns(self):
+        literal, numeral = self.position
+        turns = [
+            (chr(ord(literal) + 1), numeral + 1),
+            (chr(ord(literal) - 1), numeral - 1),
+            (chr(ord(literal) - 1), numeral + 1),
+            (chr(ord(literal) + 1), numeral - 1),
+            (chr(ord(literal)), numeral - 1),
+            (chr(ord(literal)), numeral + 1),
+            (chr(ord(literal) + 1), numeral),
+            (chr(ord(literal) - 1), numeral),
+        ]
+        return set(filter(lambda t: not self.is_out_of_board(*t), turns))
+
+
+class Queen(ChessFigure):
+    def turns(self):
+        literal, numeral = self.position
+        rook_turns = [(chr(ord(literal) - i), numeral) for i in range(1, 8)] +\
+                     [(chr(ord(literal) + i), numeral) for i in range(1, 8)] +\
+                     [(chr(ord(literal)), numeral + i) for i in range(1, 8)] +\
+                     [(chr(ord(literal)), numeral - i) for i in range(1, 8)]
+        bishop_turns = [(chr(ord(literal) - i), numeral - i) for i in range(1, 8)] +\
+                       [(chr(ord(literal) + i), numeral + i) for i in range(1, 8)] +\
+                       [(chr(ord(literal) - i), numeral + i) for i in range(1, 8)] +\
+                       [(chr(ord(literal) + i), numeral - i) for i in range(1, 8)]
+
+        return set(filter(lambda t: not self.is_out_of_board(*t), rook_turns + bishop_turns)) - {self.position}
+
+
+class Rook(ChessFigure):
+    def turns(self):
+        literal, numeral = self.position
+        turns = [(chr(ord(literal) - i), numeral) for i in range(1, 8)] +\
+                [(chr(ord(literal) + i), numeral) for i in range(1, 8)] +\
+                [(chr(ord(literal)), numeral + i) for i in range(1, 8)] +\
+                [(chr(ord(literal)), numeral - i) for i in range(1, 8)]
+
+        return set(filter(lambda t: not self.is_out_of_board(*t), turns)) - {self.position}
+
+
+class Bishop(ChessFigure):
+    def turns(self):
+        literal, numeral = self.position
+        turns = [(chr(ord(literal) - i), numeral - i) for i in range(1, 8)] +\
+                [(chr(ord(literal) + i), numeral + i) for i in range(1, 8)] +\
+                [(chr(ord(literal) - i), numeral + i) for i in range(1, 8)] +\
+                [(chr(ord(literal) + i), numeral - i) for i in range(1, 8)]
+
+        return set(filter(lambda t: not self.is_out_of_board(*t), turns)) - {self.position}
+
+
+class Knight(ChessFigure):
+
+    def turns(self):
+        literal, numeral = self.position
+        turns = [
+            (chr(ord(literal) - 2), numeral - 1),
+            (chr(ord(literal) - 2), numeral + 1),
+            (chr(ord(literal) + 2), numeral - 1),
+            (chr(ord(literal) + 2), numeral + 1),
+            (chr(ord(literal) - 1), numeral - 2),
+            (chr(ord(literal) - 1), numeral + 2),
+            (chr(ord(literal) + 1), numeral - 2),
+            (chr(ord(literal) + 1), numeral + 2),
+        ]
+        return set(filter(lambda t: not self.is_out_of_board(*t), turns))
+
+
+class Pawn(ChessFigure):
+    def turns(self):
+        literal, numeral = self.position
+        short_turn = (literal, numeral + 1)
+        long_turn = (literal, numeral + 2)
+        return {short_turn} if self.touched else {short_turn, long_turn}
+     

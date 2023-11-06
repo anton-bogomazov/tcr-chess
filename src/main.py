869154739ed38edfa10d6987e1fb9857ec834489
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-11-06 19:23:50 +0100

    TCR

--- src/main.py
+++ src/main.py
@@ -1,30 +1,20 @@
 from chess.game import standard_chess_game
-import os
+from cli import Cli
+from gui import Gui
 
 
 def main():
     game = standard_chess_game()
-    while True:
-        print_board(game.get_board().to_string())
-        turn = input('> ')
-        game.turn(*turn.split())
-        os.system('clear')
-    
+    mode = 'gui'
+    match mode:
+        case 'gui':
+            Gui(game).start()
+        case 'cli':
+            Cli(game).start()
+        case _:
+            Cli(game).start()
 
-def print_board(board):
-    print("      a     b     c     d     e     f     g     h")
-    print("   " + "+-----" * 8 + "+")
-    for i, row in enumerate(board):
-        print(8 - i, end="  ")
-        print("|", end="  ")
-        for cell in row:
-            if cell == ' ':
-                print(' ', end='')
-            print(cell, end=" |  ")
-        print(8 - i, end="")
-        print("\n   " + "+-----" * 8 + "+")
-    print("      a     b     c     d     e     f     g     h")
-    
+
 
 if __name__ == '__main__':
     main()

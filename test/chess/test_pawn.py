4a49a0eb92eec84255d1885e2aec2c2933538a1e
Anton Bogomazov <ext-abogomazov@barrage.netgit config --local user.name Anton>
2023-11-10 18:10:40 +0100

    TCR

--- test/chess/test_pawn.py
+++ test/chess/test_pawn.py
@@ -1,39 +1,41 @@
 import unittest
 from src.chess.figures import Pawn, King
 from src.chess.figures import Color
-from src.chess.sets import standard_chess_figure_set
+from src.chess.sets import standard_chess_figure_set as std_figures
 
 
 class PawnTest(unittest.TestCase):
     
     def test_white_pawn_possible_turns(self):
         self.assertEqual(
-            white_pawn().turns(standard_chess_figure_set()), {('a', 3), ('a', 4)}
+            _pawn().turns(std_figures()),
+            {('a', 3), ('a', 4)}
         )
 
     def test_black_pawn_possible_turns(self):
         self.assertEqual(
-            Pawn(('a', 7), Color.BLACK).turns(standard_chess_figure_set()), {('a', 6), ('a', 5)}
+            _pawn(('a', 7), Color.BLACK).turns(std_figures()),
+            {('a', 6), ('a', 5)}
         )
 
     def test_two_different_pawns_are_not_eq(self):
         self.assertNotEqual(
-            Pawn(('a', 2), Color.WHITE),
-            Pawn(('a', 2), Color.BLACK)
+            _pawn(('a', 2), Color.WHITE),
+            _pawn(('a', 2), Color.BLACK)
         )
 
     def test_pawn_possible_turns_only_short_turn_after_touch(self):
-        pawn = white_pawn()
+        pawn = _pawn()
         pawn.move(('a', 3))
         self.assertEqual(
             pawn.turns(), {('a', 4)}
         )
 
     def test_pawn_move(self):
-        pawn = white_pawn()
+        pawn = _pawn()
         pawn.move(('a', 3))
         self.assertEqual(pawn.position, ('a', 3))
 
 
-def white_pawn(position=('a', 2), color=Color.WHITE):
+def _pawn(position=('a', 2), color=Color.WHITE):
     return Pawn(position, color)

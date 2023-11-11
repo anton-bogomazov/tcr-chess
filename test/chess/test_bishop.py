0e6908a324516f871e4f5a599e9ede2211bbe079
Anton Bogomazov <ext-abogomazov@barrage.net>
2023-11-11 16:26:18 +0100

    TCR

---
+++ test/chess/test_bishop.py
@@ -0,0 +1,18 @@
+import unittest
+from src.chess.figures.bishop import Bishop
+from src.chess.figures.color import Color
+
+
+class BishopTest(unittest.TestCase):
+    
+    def test_bishop_possible_turns(self):
+        bishop = _bishop()
+        
+        self.assertEqual(bishop.turns(), {
+            ('a', 1), ('b', 2), ('d', 2), ('e', 1), ('f', 6),
+            ('a', 5), ('b', 4), ('d', 4), ('e', 5), ('g', 7), ('h', 8),
+        })
+        
+
+def _bishop(position=('c', 3), color=Color.WHITE):
+    return Bishop(position, color)

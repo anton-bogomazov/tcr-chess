1cbead4d4d2d84dc5867bd1e2c30ac0eca8ffd06
Anton Bogomazov <ext-abogomazov@barrage.netgit config --local user.name Anton>
2023-11-11 16:40:52 +0100

    TCR

--- src/chess/figures/queen.py
+++ src/chess/figures/queen.py
@@ -1,20 +1,16 @@
+from src.chess.board_utils import inc_num_pos as up, dec_num_pos as down, dec_lit_pos as left, inc_lit_pos as right, cell, position
 from src.chess.figures.chess_figure import ChessFigure
 from src.chess.figures.color import Color
 
 
 class Queen(ChessFigure):
     def turns(self, figures=frozenset()):
-        literal, numeral = self.position
-        rook_turns = [(chr(ord(literal) - i), numeral) for i in range(1, 8)] +\
-                     [(chr(ord(literal) + i), numeral) for i in range(1, 8)] +\
-                     [(chr(ord(literal)), numeral + i) for i in range(1, 8)] +\
-                     [(chr(ord(literal)), numeral - i) for i in range(1, 8)]
-        bishop_turns = [(chr(ord(literal) - i), numeral - i) for i in range(1, 8)] +\
-                       [(chr(ord(literal) + i), numeral + i) for i in range(1, 8)] +\
-                       [(chr(ord(literal) - i), numeral + i) for i in range(1, 8)] +\
-                       [(chr(ord(literal) + i), numeral - i) for i in range(1, 8)]
+        turns = self.calc_moves(figures, [up, left]) + self.calc_moves(figures, [down, left]) +\
+                self.calc_moves(figures, [up, right]) + self.calc_moves(figures, [down, right]) +\
+                self.calc_moves(figures, [up]) + self.calc_moves(figures, [down]) +\
+                self.calc_moves(figures, [left]) + self.calc_moves(figures, [right])
 
-        return set(filter(lambda t: not self.is_out_of_board(*t), rook_turns + bishop_turns)) - {self.position}
+        return set(turns)
 
     def notation(self):
         return 'Q'

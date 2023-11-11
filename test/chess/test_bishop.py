28041777ce0251054f8f987a434bd3de13cf4908
Anton Bogomazov <ext-abogomazov@barrage.netgit config --local user.name Anton>
2023-11-11 17:18:41 +0100

    TCR

--- test/chess/test_bishop.py
+++ test/chess/test_bishop.py
@@ -8,7 +8,7 @@ class BishopTest(unittest.TestCase):
     def test_bishop_possible_turns(self):
         bishop = _bishop()
         
-        self.assertEqual(bishop.turns(), {
+        self.assertEqual(bishop.turns([]), {
             ('a', 1), ('b', 2), ('d', 2), ('e', 1), ('f', 6),
             ('a', 5), ('b', 4), ('d', 4), ('e', 5), ('g', 7), ('h', 8),
         })

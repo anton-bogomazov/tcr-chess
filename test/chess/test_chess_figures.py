d023aa4defc2fdbadd0fe219289a054b21777913
Anton Bogomazov <ext-abogomazov@barrage.netgit config --local user.name Anton>
2023-11-11 13:36:27 +0100

    TCR

--- test/chess/test_chess_figures.py
+++ test/chess/test_chess_figures.py
@@ -44,14 +44,6 @@ class ChessFiguresTest(unittest.TestCase):
             }
         ))
 
-    def test_king_possible_turns(self):
-        king = white_king()
-        self.assertEqual(len(king.turns()), 8)
-        self.assertEqual(king.turns(), {
-            ('b', 3), ('c', 2), ('b', 2), ('d', 4),
-            ('d', 3), ('c', 4), ('d', 2), ('b', 4)
-        })
-
 
 
 def white_queen():
--- test/chess/test_king.py
+++ test/chess/test_king.py
@@ -5,7 +5,14 @@ from src.chess.king import King
 
 
 class KingTest(unittest.TestCase):
-    ...
+    
+    def test_king_possible_turns(self):
+        king = white_king()
+        self.assertEqual(len(king.turns()), 8)
+        self.assertEqual(king.turns(), {
+            ('b', 3), ('c', 2), ('b', 2), ('d', 4),
+            ('d', 3), ('c', 4), ('d', 2), ('b', 4)
+        })
 
 
 def white_king():

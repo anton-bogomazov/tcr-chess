dbb6cd73874ab0c7ea30ebc2fcfb4b1321a2e9fe
Anton Bogomazov <ext-abogomazov@barrage.netgit config --local user.name Anton>
2023-11-12 21:23:06 +0100

    TCR

--- src/chess/figures/bishop.py
+++ src/chess/figures/bishop.py
@@ -7,9 +7,7 @@ from functools import reduce
 class Bishop(ChessFigure):
     def turns(self, figures):
         turns = [up, left], [down, left], [up, right], [down, right]
-        turns = reduce(lambda acc, m: acc + self.calc_moves(figures, m), turns, [])
-
-        return set(turns)
+        return set(reduce(lambda acc, m: acc + self.calc_moves(figures, m), turns, []))
 
     def notation(self):
         return 'B'

405a431c2c3001771bb33732e6c890743202dd91
Anton Bogomazov <ext-abogomazov@barrage.netgit config --local user.name Anton>
2023-11-12 19:15:12 +0100

    TCR

--- src/chess/board.py
+++ src/chess/board.py
@@ -19,7 +19,7 @@ class ChessBoard:
         elif dest_cell_content is None:
             self.moving(to, figure_to_move)
         elif isinstance(dest_cell_content, ChessFigure):
-            self.take(to, figure_to_move, dest_cell_content)
+            self.take(to, figure_to_move)
         else:
             raise InconsistentStateError('something else except None or Figure in the cell')
         
@@ -32,7 +32,8 @@ class ChessBoard:
         if figure_to_move.is_transformable_pawn():
             self.transform_pawn(figure_to_move)
         
-    def take(self, to, figure_to_move, dest_figure):
+    def take(self, to, figure_to_move):
+        dest_figure = self.cell(*to)
         if dest_figure.color != figure_to_move.color:
             if to not in figure_to_move.turns(self.figures):
                 raise InvalidMoveError()

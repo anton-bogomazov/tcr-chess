78f3a15d65ea1f3c57871a5918acdf366465608f
Anton Bogomazov <ext-abogomazov@barrage.netgit config --local user.name Anton>
2023-11-12 21:18:12 +0100

    TCR

--- src/chess/figures/rook.py
+++ src/chess/figures/rook.py
@@ -12,20 +12,20 @@ class Rook(ChessFigure):
         return set(turns)
 
     def castle(self, kings_to):
-        self.move(self.rook_castling_to(kings_to))
-
-    def rook_castling_to(self, to):
-        castling_map = {
-            ('g', 1): ('f', 1),
-            ('c', 1): ('d', 1),
-            ('g', 8): ('f', 8),
-            ('c', 8): ('d', 8),
-        }
-        return castling_map[to]
+        self.move(rook_castling_to(kings_to))
 
     def notation(self):
         return 'R'
 
     def symbol(self):
         return '\u2656' if self.color == Color.WHITE else '\u265C'
-            
+          
+          
+def rook_castling_to(to):
+    castling_map = {
+        ('g', 1): ('f', 1),
+        ('c', 1): ('d', 1),
+        ('g', 8): ('f', 8),
+        ('c', 8): ('d', 8),
+    }
+    return castling_map[to]

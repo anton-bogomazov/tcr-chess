8f75005a12836ca5938125bda9b3917dce443068
Anton Bogomazov <ext-abogomazov@barrage.netgit config --local user.name Anton>
2023-11-26 15:36:23 +0100

    check winner in approval test

--- test/test_approval.py
+++ test/test_approval.py
@@ -1,9 +1,9 @@
 import unittest
 from src.chess.game import standard_chess_game
 from src.chess.figures.color import Color
-from src.chess.error import *
 
 import pandas as pd
+import chess.pgn
 import chess as pgn_chess
 import io
 
@@ -13,42 +13,33 @@ class ChessGameProvider:
         self.df = pd.read_csv(ds_path)
 
     def get_next(self):
-        games_moves = self.df[['moves']].sample(1)
+        games_moves = self._mate_games()[['moves', 'winner']].sample(1)
         moves = games_moves['moves'].iloc[0].split()
         enumerated_moves = [f'{i + 1}. {turn}' for i, turn in enumerate(moves)]
         parsed_game = pgn_chess.pgn.read_game(io.StringIO(' '.join(enumerated_moves)))
         uci_moves = list(map(lambda m: m.uci(), parsed_game.mainline_moves()))
         tupled_moves = list(map(lambda m: (m[:2], m[2:]), uci_moves))
 
-        return tupled_moves
+        return tupled_moves, games_moves.iloc[0]['winner']
+
+    def get_n(self, n):
+        return [self.get_next() for _ in range(n)]
+
+    def _mate_games(self):
+        return self.df[self.df['victory_status'] == 'mate']
 
 
 class ApprovalTest(unittest.TestCase):
 
-    def test_play_a_short_chess_game(self):
-        game = standard_chess_game()
-        moves = [
-            ("f2", "f3"),
-            ("e7", "e5"),
-            ("g2", "g4"),
-            ("d8", "h4"), # checkmate
-        ]
-        for move in moves:
-            game.turn(*move)
-
-        self.assertEqual(Color.WHITE, game.__checked_player)
-        
-        with self.assertRaises(CheckmateError):
-            game.turn("f3", "f4")
-
-        self.assertEqual(True, game.__checkmate)
-
+    # it fails sometimes because of lacking en passant capturing implementation
     def test_play_game(self):
         game_provider = ChessGameProvider()
 
-        while True:
+        for (game_moves, winner) in game_provider.get_n(10):
             game = standard_chess_game()
-            moves = game_provider.get_next()
-            print(moves)
-            for move in moves:
+            print(game_moves)
+            for move in game_moves:
                 game.turn(*move)
+
+            opponent_color = Color.BLACK if Color[winner.upper()] == Color.WHITE else Color.WHITE
+            self.assertEqual(opponent_color, game.checked_player())

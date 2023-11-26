8826be548e8eb41ce3377d2830cae14d891ceba6
Anton Bogomazov <ext-abogomazov@barrage.netgit config --local user.name Anton>
2023-11-26 15:04:49 +0100

    TCR

--- test/test_approval.py
+++ test/test_approval.py
@@ -3,6 +3,25 @@ from src.chess.game import standard_chess_game
 from src.chess.figures.color import Color
 from src.chess.error import *
 
+import pandas as pd
+import chess as pgn_chess
+import io
+
+
+class ChessGameProvider:
+    def __init__(self, ds_path='games.csv'):
+        self.df = pd.read_csv(ds_path)
+
+    def get_next(self):
+        games_moves = self.df[['moves']].sample(1)
+        moves = games_moves['moves'].iloc[0].split()
+        enumerated_moves = [f'{i + 1}. {turn}' for i, turn in enumerate(moves)]
+        parsed_game = pgn_chess.pgn.read_game(io.StringIO(' '.join(enumerated_moves)))
+        uci_moves = list(map(lambda m: m.uci(), parsed_game.mainline_moves()))
+        tupled_moves = list(map(lambda m: (m[:2], m[2:]), uci_moves))
+
+        return tupled_moves
+
 
 class ApprovalTest(unittest.TestCase):
 
@@ -23,4 +42,13 @@ class ApprovalTest(unittest.TestCase):
             game.turn("f3", "f4")
 
         self.assertEqual(True, game.__checkmate)
-        
+
+    def test_play_game(self):
+        game_provider = ChessGameProvider()
+
+        while True:
+            game = standard_chess_game()
+            moves = game_provider.get_next()
+            print(moves)
+            for move in moves:
+                game.turn(*move)
